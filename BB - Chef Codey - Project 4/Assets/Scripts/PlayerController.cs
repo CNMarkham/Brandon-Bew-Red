f256ef92f5ce1876da3d79797b5719177a3b76e7
Ryan <markhamonca@codeninjas.com>
2024-05-29 18:00:15 -0400

    only rotate left and right, build more

---
+++ BB - Chef Codey - Project 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class PlayerController : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody rb;
+    private Quaternion lastLook;
+    public float speed;
+    public CinemachineVirtualCameraBase vCam;
+    [SerializeField] private int rotateAmount = 45;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        anim = GetComponent<Animator>();
+        lastLook = transform.rotation;
+        vCam = FindObjectOfType<CinemachineVirtualCameraBase>();
+    }
+
+    void Update()
+    {
+        var cam = Camera.main.transform.TransformVector(transform.forward).normalized;
+        var newforward = new Vector3(cam.x, 0, cam.z);
+
+        //velocity.y += gravity * Time.deltaTime;
+
+        var forward = Input.GetAxis("Vertical");
+        var sideward = Input.GetAxis("Horizontal");
+
+        var direction = newforward * forward + Camera.main.transform.TransformVector(transform.right).normalized * sideward;
+
+
+        // Vector3 movement = new Vector3(horizontal, 0, vertical)  * speed / 100;
+
+        rb.MovePosition( transform.position + direction * speed * Time.deltaTime);
+
+       // vCam.
+    }
+}
